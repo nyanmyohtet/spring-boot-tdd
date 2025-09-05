f1bd1bf756044f2d687cfb3bcc23a2dea0efa49b
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2025-09-05 18:33:25 +0800

    init

---
+++ src/test/java/com/nyanmyohtet/tdd/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyanmyohtet.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
